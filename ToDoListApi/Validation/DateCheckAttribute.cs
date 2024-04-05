e47c63785528a989e3f721f04b558d43dbd6d99f
eaycayildirim <eaycayildirim@gmail.com>
2024-04-05 13:46:45 +0200

    Added custom validations

---
+++ ToDoListApi/Validation/DateCheckAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApi.Validation
+{
+    public class DateCheckAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var date = (DateTime?)value;
+            if (date < DateTime.Now)
+                return new ValidationResult("Due date must require a future date to be selected.");
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ ToDoListApi/Validation/UniqueIdAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApi.Validation
+{
+    public class UniqueIdAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var id = (int?)value;
+            if (id <= 0)
+                return new ValidationResult("Id must be greater than zero");
+            return ValidationResult.Success;
+        }
+    }
+}
